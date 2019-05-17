81e0e07fad2d956d396483f24e10a24f9dec1d2b
Sai Medury <sai-medury@mocs.utc.edu>
2019-05-17 12:01:01 -0400

    1. Added CMakeLists 2. Simple solution to sort using heap and sort_heap 3. Added Gooogle tests

--- day-0/sort-k-sorted-lists-each-size-n.h
+++ day-0/sort-k-sorted-lists-each-size-n.h
@@ -1,9 +1,16 @@
 #include <vector>
+#include <stdexcept>
+#include <algorithm>
 
 std::vector<int> sort_k_lists(const int& k,
-    const vector<int>& input_list) {
+    const std::vector<int>& input_list) {
+  std::vector<int> output_list = std::vector<int>(input_list);
   // Assuming that each list is of same size, 
   // otherwise we need n
-  int size_each_list = input_list.size()/k;
-
+  if( input_list.size()%k != 0){
+    throw std::invalid_argument("List is not uniform \n");
+  }
+  make_heap(output_list.begin(), output_list.end());
+  sort_heap(output_list.begin(), output_list.end());
+  return output_list;
 }
--- day-0/tests.cpp
+++ day-0/tests.cpp
@@ -1,10 +1,26 @@
-#include "gtest/gtest.h"
 #include "sort-k-sorted-lists-each-size-n.h"
+#include "gtest/gtest.h"
 
-namespace {
-  TEST(SortTest, Simple) {
-    std::vector<int> input_list = { 10, 15, 30, 12, 15, 20, 17, 20, 32 };
-    std::vector<int> output_list = sort_k_lists(3, input_list);
-    EXPECT_TRUE(std::is_sorted(std::begin(output_list), std::end(output_list)));
+TEST(SortTest, Simple){
+  std::vector<int> input_list = { 10, 15, 30, 12, 15, 20, 17, 20, 32 };
+  std::vector<int> output_list = sort_k_lists(3, input_list);
+  EXPECT_TRUE(std::is_sorted(std::begin(output_list), std::end(output_list)));
+}
+
+TEST(SortTest, LargeVector){
+  int k(10), n(100);
+  std::vector<int> input_list = std::vector<int>(n,0);
+  srand(time(0));
+  for(int i=0; i<n; i=i+k){
+    for(int j=i; j<i+k; j++){
+      input_list[j]=rand()%100;
+    }
   }
+  std::vector<int> output_list = sort_k_lists(k, input_list);
+  EXPECT_TRUE(std::is_sorted(std::begin(output_list), std::end(output_list)));
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
